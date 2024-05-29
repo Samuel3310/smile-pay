1cebec185bcabeee1c8c0f69e5b5859f133ce023
samuel3310 <awoluowolabisamuel@gmail.com>
2024-05-29 13:04:03 +0100

    created a test api route and controller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -18,3 +19,5 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
+
+app.use("/api/user", userRoute);
