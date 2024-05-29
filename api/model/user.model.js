3048953bc7a443adb7d8a08b51895ae8a03e3cc9
samuel3310 <awoluowolabisamuel@gmail.com>
2024-05-29 12:34:02 +0100

    User Model Created

---
+++ api/model/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    number: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
